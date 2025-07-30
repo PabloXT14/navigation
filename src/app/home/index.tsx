83ce7014ce0bca4dd921fbcd6d3a9b7d2bbcd232
Pablo Alan <pabloxt14@gmail.com>
2025-07-30 16:11:10 -0300

    feat: navigate between screens with useNavigation

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,16 +1,23 @@
 import { StyleSheet, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 import { Header } from '@/components/header'
 import { Title } from '@/components/title'
 import { ButtonIcon } from '@/components/button-icon'
 
 export function Home() {
+  const navigation = useNavigation()
+
+  function handleNavigateToProduct() {
+    navigation.navigate('product')
+  }
+
   return (
     <View style={styles.container}>
       <Header>
         <Title>Home</Title>
 
-        <ButtonIcon name="add-circle" />
+        <ButtonIcon name="add-circle" onPress={handleNavigateToProduct} />
       </Header>
     </View>
   )
--- src/app/product/index.tsx
+++ src/app/product/index.tsx
@@ -1,14 +1,21 @@
 import { StyleSheet, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 import { Header } from '@/components/header'
 import { Title } from '@/components/title'
 import { ButtonIcon } from '@/components/button-icon'
 
 export function Product() {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
     <View style={styles.container}>
       <Header>
-        <ButtonIcon name="arrow-circle-left" />
+        <ButtonIcon name="arrow-circle-left" onPress={handleGoBack} />
 
         <Title>Product</Title>
       </Header>
